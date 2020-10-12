0bd37f6ab65644f118587f2cd14210cd35869933
wmatioli <wagner.matioli@gmail.com>
2020-10-12 19:32:50 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getMonth());
+alert(d.getUTCMinutes());
+
+
+/*
+var count;
+for(count=0; count<= 5; count++){
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+    //count = count + 1;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.pop();
+//lista.push("uva");
+//console.log(lista);
+//console.log(lista[1]);
+//alert(lista[1]);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - ")); 
+
+
+
+//var nome = "Wagner Matioli";
+//var idade = 45;
+//var idade2 = 10;
+//var frase = "Alemanha é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase);
+//console.log(frase.toLowerCase());
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Alemanha", "Brasil"));
+//alert(frase.replace("Alemanha", "Brasil"));
+
+
