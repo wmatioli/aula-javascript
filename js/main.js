8ee1ced5568e53ef06d600e84ed75fd3069f2807
wmatioli <wagner.matioli@gmail.com>
2020-10-12 22:11:40 -0300

    .\index.html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
 
+function clicou(){
+    alert("Obrigado por clicar");
+}
 
+
+
+/*  
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+var validar;
+function validaIdade(idade){
+    validar;
+    if (idade >= 18){
+        validar = true;
+    }else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+validaIdade(idade);
+console.log(validar);
+
+
+//alert(soma(5, 10));
+*/
+
+/*
 var d = new Date();
 alert(d.getDay());
 alert(d.getMonth());
 alert(d.getUTCMinutes());
+*/
 
 
 /*
